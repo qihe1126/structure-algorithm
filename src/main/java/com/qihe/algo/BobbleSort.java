b260a3c885bdcf8bf9c15e21577a98619bfca282
qihe <qihea163@163.com>
2023-04-21 13:33:05 +0800

    二分查找递归

---
+++ src/main/java/com/qihe/algo/BobbleSort.java
@@ -0,0 +1,19 @@
+package com.qihe.algo;
+
+public class BobbleSort {
+
+    /**
+     * 冒泡排序
+     *
+     * @param array 数组
+     * @param target 查找目标
+     * @return 1未找到 >-1 是找到的数组下标
+     */
+    public static int binarySearchBasic(int[] array, int target) {
+
+        return -1;
+    }
+
+
+
+}
